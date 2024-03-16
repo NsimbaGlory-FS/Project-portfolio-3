1b572b8a96b06d163ece11fca0f0c69cef4b57f0
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-03-16 10:15:21 -0400

    add to README

--- spotify-backend/index.js
+++ spotify-backend/index.js
@@ -8,11 +8,11 @@ const authRoutes = require("./routes/auth");
 const songRoutes = require("./routes/song");
 const playlistRoutes = require("./routes/playlist");
 require("dotenv").config();
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 mongoose
--- spotify-backend/routes/auth.js
+++ spotify-backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const { getToken } = require("../utils/helpers");
+const { getToken } = require("../utils/index");
 
 router.post("/register", async (req, res) => {
   const { email, password, firstName, lastName, username } = req.body;
