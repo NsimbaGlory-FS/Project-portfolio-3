880b95fc0fbed10b8d6b102bc2be6d3ee6488371
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-03-23 00:31:27 -0400

    add

--- spotify/pages/index.js
+++ spotify/pages/index.js
@@ -1,3 +1,8 @@
+import Artist from "../components/Artist";
+import Library from "../components/Library";
+import Player from "../components/Player";
+import PlaylistView from "../components/PlaylistView";
+import Search from "../components/Search";
 import Sidebar from "../components/Sidebar";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -17,6 +22,47 @@ export default function Home() {
             view={view}
             setView={setView}
             setGlobalPlaylistId={setGlobalPlaylistId}
+          />
+          {view === "playlist" && (
+            <PlaylistView
+              setView={setView}
+              setGlobalArtistId={setGlobalArtistId}
+              globalPlaylistId={globalPlaylistId}
+              setGlobalCurrentSongId={setGlobalCurrentSongId}
+              setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+            />
+          )}
+          {view === "search" && (
+            <Search
+              setView={setView}
+              setGlobalPlaylistId={setGlobalPlaylistId}
+              setGlobalCurrentSongId={setGlobalCurrentSongId}
+              setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+              setGlobalArtistId={setGlobalArtistId}
+            />
+          )}
+          {view === "library" && (
+            <Library
+              setView={setView}
+              setGlobalPlaylistId={setGlobalPlaylistId}
+            />
+          )}
+          {view === "artist" && (
+            <Artist
+              setView={setView}
+              globalArtistId={globalArtistId}
+              setGlobalArtistId={setGlobalArtistId}
+              setGlobalCurrentSongId={setGlobalCurrentSongId}
+              setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+            />
+          )}
+        </div>
+        <div className="sticky z-20 bottom-0 w-full">
+          <Player
+            globalCurrentSongId={globalCurrentSongId}
+            setGlobalCurrentSongId={setGlobalCurrentSongId}
+            setGlobalIsTrackPlaying={setGlobalIsTrackPlaying}
+            globalIsTrackPlaying={globalIsTrackPlaying}
           />
         </div>
       </main>
--- spotify/pages/login.js
+++ spotify/pages/login.js
@@ -4,9 +4,14 @@ const Login = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <button
-        className="text-white px-8 py-2 rounded-full bg-green-500 font-bold text-lg"
+        className="text-white px-5 py-3 rounded-full bg-green-500 font-bold text-lg"
         onClick={() => signIn("spotify", { callbackUrl: "/" })}
       >
+        <img
+          src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
+          alt="spotify"
+          width={900}
+        ></img>
         Login to Spotify
       </button>
     </div>
